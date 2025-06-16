5c6fc5997aa002b78129c5d9867470a5b25617b9
A_95 <a95lopezalejandro@gmail.com>
2025-06-16 13:09:36 -0600

    Update show.blade.php

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -4,15 +4,14 @@
     <div class="container">
         <h1 class="mb-3">{{ $book->title }}</h1>
         <p class="text-muted">{{ $book->description }}</p>
-
+        <a href="{{ asset('storage/' . $book->pdf_path) }}" target="_blank" class="btn btn-outline-primary">📄 Ver en otra página</a>
         <div class="mt-4">
             <div class="ratio ratio-4x3">
                 <embed src="{{ asset('storage/' . $book->pdf_path) }}" type="application/pdf" class="rounded shadow-sm" />
             </div>
         </div>
         <br>
-        <a href="{{ asset('storage/' . $book->pdf_path) }}" target="_blank" class="btn btn-outline-primary">📄 Ver en otra
-            página</a>
+        
 
     </div>
 @endsection
